ebbde16426501b29ca5c133a0b3053238e2eb17d
HarshitaV-git <82379884+HarshitaV-git@users.noreply.github.com>
2021-04-19 13:20:12 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package p1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+
+	}
+
+}
+
+
+
